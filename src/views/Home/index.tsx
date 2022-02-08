ba88c0f1dd00341f5209f0a8511a30fffdbe736c
shaolinmkz <nwabuzor.obiora@gmail.com>
2022-02-08 11:23:34 +0100

    fix loader arrangment

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -41,6 +41,7 @@ const Home = () => {
           <>
             <BooksLoader />
             <BooksLoader />
+            <BooksLoader />
           </>
         ) : (
           books.map((book: IBooks) => (
